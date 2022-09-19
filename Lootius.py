39834826fd75ac48dacf01b6f4385d88f902dc0b
Nikander van der Schee <nvan-der@student.codam.nl>
2022-09-19 03:09:10 +0200

    cleanup

--- Lootius.py
+++ Lootius.py
@@ -6,5 +6,5 @@ def	customLogic(window):
 	window.toggle_fullscreen()
 	window.evaluate_js('alert("Nice one brother")')
 
-window = webview.create_window("Lootius", "./views/index.html")
+window = webview.create_window("Lootius", "")
 webview.start(customLogic, window, gui="cef", http_server=True, debug=True)
