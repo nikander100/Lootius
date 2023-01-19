50f8ae2e0d4bb906dc609c7485ecf6e28237a025
Nikander van der Schee <nvan-der@student.codam.nl>
2023-01-19 15:48:51 +0100

    Update Lootius.py

--- Lootius.py
+++ Lootius.py
@@ -1,10 +1,1 @@
-import webview
-import threading
 
-
-def	customLogic(window):
-	window.toggle_fullscreen()
-	window.evaluate_js('alert("Nice one brother")')
-
-window = webview.create_window("Lootius", "")
-webview.start(customLogic, window, gui="cef", http_server=True, debug=True)
