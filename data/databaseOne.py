4d7eaff71e37f58dc80f3c0de8e80afc4b25a51b
Nikander van der Schee <ndvdschee@gmail.com>
2023-03-16 19:54:40 +0100

    Update databaseOne.py
    Made class also for during dev drop db (my thrash had like 200 test.db files :100:

--- data/databaseOne.py
+++ data/databaseOne.py
@@ -3,7 +3,24 @@ import csv, time, sqlalchemy, typing
 from databaseModel import Base
 from sqlite3 import Error
 
-lootiusDB = Base()
-engine = sqlalchemy.create_engine("sqlite+pysqlite:///data/lootiusTest.db", echo=True)
-lootiusDB.metadata.create_all(engine)
-# lootiusDB.metadata.drop_all(engine)
+class SetupDatabase:
+    def __init__(self):
+        pass
+
+    def _dropDatabase():
+        lootiusDB = Base()
+        engine = sqlalchemy.create_engine("sqlite+pysqlite:///data/lootiusTest.db", echo=True)
+        lootiusDB.metadata.drop_all(engine)
+        
+
+    def setupDatabse(dbFilePath):
+        lootiusDB = Base()
+        try:
+            engine = sqlalchemy.create_engine(f"sqlite+pysqlite://{dbFilePath}", echo=True)
+        except Error as e:
+            print(e)  ;"""log to errors, have to find out what the error return is from alchemy"""
+        finally:
+            lootiusDB.metadata.create_all(engine)
+
+SetupDatabase.setupDatabse("/data/lootiusTest.db")
+# SetupDatabase._dropDatabase()
