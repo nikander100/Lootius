aa836385ce9165317e34f048f0d460ffac53a823
Nikander van der Schee <ndvdschee@gmail.com>
2023-03-18 17:02:42 +0100

    csv to sql working with pd.

--- lootius/database/databaseOne.py
+++ lootius/database/databaseOne.py
@@ -1,6 +1,6 @@
 import sqlite3
-import csv, time, sqlalchemy, typing, sys
-from lootius.models.databaseModel import Base
+import pandas, time, sqlalchemy, typing, sys
+from lootius.models.databaseModel import *
 from sqlite3 import Error
 
 class SetupDatabase:
@@ -13,6 +13,16 @@ class SetupDatabase:
         lootiusDB.metadata.drop_all(engine)
         
 
+    def __populateDatabase(self, engine):
+
+        # Populate weapons
+        weaponsCSV = "./data/csv/weapons.csv"
+        convert = (lambda x: 3 if x == "Melee" else 2)
+        df = pandas.read_csv(weaponsCSV, sep=";", converters={"Class":convert})
+        df = df.rename(columns={"Name":"name", "Class":"weaponTypeID", "Damage":"damage", "Attacks":"firerate", "Decay":"decay", "Ammo":"ammoBurn"})
+        df = df.replace(r'^\s*$', 0, regex=True)
+        df.to_sql(con=engine, name=Weapons.__tablename__, if_exists="append", index=False)
+
     def setupDatabase(self, dbFilePath):
         lootiusDB = Base()
         try:
@@ -21,8 +31,12 @@ class SetupDatabase:
             print(e)  ;"""log to errors, have to find out what the error return is from alchemy"""
         finally:
             lootiusDB.metadata.create_all(engine)
+            self.__populateDatabase(engine)
+
+
 
 testDB = SetupDatabase()
+testDB._dropDatabase()
+time.sleep(1)
 testDB.setupDatabase("/lootius/database/lootiusTest.db")
-# time.sleep(1)
-# testDB._dropDatabase()
+
