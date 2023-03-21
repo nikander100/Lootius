774909995c2b7123b623d21528c2ed8a2660e3db
Nikander van der Schee <ndvdschee@gmail.com>
2023-03-21 23:42:28 +0100

    more db types added etc.

--- lootius/database/databaseOne.py
+++ lootius/database/databaseOne.py
@@ -17,19 +17,31 @@ class SetupDatabase:
         Session = sessionmaker(engine)
         from lootius.models.databaseModel import WeaponTypes
         with Session.begin() as session:
-            wType1, wType2, wType3 = (WeaponTypes() for _ in range(3))
-            wType1.type = "unknown"
-            wType2.type = "ranged"
-            wType3.type = "melee"
-            session.add_all([wType1, wType2, wType3])
+            typeIdOne, typeIdTwo, typeIdThree, typeIdFour, typeIdFive = (WeaponTypes() for _ in range(5))
+            typeIdOne.type = "all"
+            typeIdTwo.type = "laser"
+            typeIdThree.type = "blp"
+            typeIdFour.type = "mindforce"
+            typeIdFive.type = "melee"
+            session.add_all([typeIdOne, typeIdTwo, typeIdThree, typeIdFour, typeIdFive])
+
 
     def __populateWeaponTable(self, engine):
         from lootius.models.databaseModel import Weapons
         weaponsCSV = "./data/csv/weapons.csv"
-        convert = (lambda x: 3 if x == "Melee" else 2)
-        df = pandas.read_csv(weaponsCSV, sep=";", converters={"Class":convert})
-        df = df.rename(columns={"Name":"name", "Class":"weaponTypeID", "Damage":"damage", "Attacks":"firerate", "Decay":"decay", "Ammo":"ammoBurn"})
+        laserSet = {"laser", "gauss"}
+        blpSet = {"blp", "plasma"}
+        mindforceSet = {"cryogenic", "electric", "pyro"}
+        meleeSet = {"axes", "clubs", "longblades", "power fist", "shortblades", "whip"}
+        convert = (lambda x: 5 if str(x).lower() in meleeSet \
+                    else 4 if str(x).lower() in mindforceSet \
+                    else 3 if str(x).lower() in blpSet \
+                    else 2 if str(x).lower() in laserSet \
+                    else 1)
+        df = pandas.read_csv(weaponsCSV, sep=";", converters={"Type":convert})
+        df = df.rename(columns={"Name":"name", "Type":"weaponTypeID", "Damage":"damage", "Attacks":"firerate", "Decay":"decay", "Ammo":"ammoBurn"})
         df = df.replace(r'^\s*$', 0, regex=True)
+        df = df.fillna(0)
         df.to_sql(con=engine, name=Weapons.__tablename__, if_exists="append", index=False)
 
     def __populateSightsTable(self, engine):
@@ -39,22 +51,42 @@ class SetupDatabase:
         df = df.rename(columns={"Name":"name", "Type":"weaponTypeID", "Decay":"decay"}) # same as in scopes
         df[["weaponTypeID"]] = df[["weaponTypeID"]].replace("Sight", 2) # same as in scopes
         df = df.replace(r'^\s*$', 0, regex=True)
+        df = df.fillna(0)
         df.to_sql(con=engine, name=Sights.__tablename__, if_exists="append", index=False)
 
     def __populateScopesTable(self, engine):
         from lootius.models.databaseModel import Scopes
-        scopesCSV = "./data/csv/sights.csv"
+        scopesCSV = "./data/csv/scopes.csv"
         df = pandas.read_csv(scopesCSV, sep=";")
         df = df.rename(columns={"Name":"name", "Type":"weaponTypeID", "Decay":"decay"}) # same as in sights
-        df[["weaponTypeID"]] = df[["weaponTypeID"]].replace("Sight", 2) # same as in sights
+        df[["weaponTypeID"]] = df[["weaponTypeID"]].replace("Scope", 2) # same as in sights
         df = df.replace(r'^\s*$', 0, regex=True)
+        df = df.fillna(0)
         df.to_sql(con=engine, name=Scopes.__tablename__, if_exists="append", index=False)
 
-    def __populateTable(self, engine, Session):
-        pass
+    def __populateWeaponAmpsTable(self, engine):
+        from lootius.models.databaseModel import WeaponAmps
+        ampsCSV = "./data/csv/weaponAmps.csv"
+        convert = (lambda x: 5 if str(x).lower() == "melee amp" \
+                    else 4 if str(x).lower() in "mf amp" \
+                    else 3 if str(x).lower() in "blp amp" \
+                    else 2 if str(x).lower() in "energy amp" \
+                    else 1)
+        df = pandas.read_csv(ampsCSV, sep=";", converters={"Type":convert})
+        df = df.rename(columns={"Name":"name", "Type":"weaponTypeID", "Decay":"decay", "Ammo":"ammoBurn"}) #
+        df = df.replace(r'^\s*$', 0, regex=True)
+        df = df.fillna(0)
+        df.to_sql(con=engine, name=WeaponAmps.__tablename__, if_exists="append", index=False)
 
-    def __populateTable(self, engine, Session):
-        pass
+    def __populateAbsorberTable(self, engine):
+        from lootius.models.databaseModel import WeaponAbsorbers
+        absorbersCSV = "./data/csv/absorbers.csv"
+        convert = (lambda x: 5 if str(x).lower() == "melee absorber" else 1)
+        df = pandas.read_csv(absorbersCSV, sep=";", converters={"Type":convert})
+        df = df.rename(columns={"Name":"name", "Type":"weaponTypeID", "Decay":"decay", "AbsorbPercent":"absorbPercent"})
+        df = df.replace(r'^\s*$', 0, regex=True)
+        df = df.fillna(0)
+        df.to_sql(con=engine, name=WeaponAbsorbers.__tablename__, if_exists="append", index=False)
 
 
     def __populateDatabase(self, engine):
@@ -70,6 +102,12 @@ class SetupDatabase:
 
         # Populate scopes
         self.__populateScopesTable(engine)
+
+        # Populate amps
+        self.__populateWeaponAmpsTable(engine)
+
+        # Populate absorbers
+        self.__populateAbsorberTable(engine)
 
 
     def setupDatabase(self, dbFilePath):
@@ -88,4 +126,3 @@ testDB = SetupDatabase()
 testDB._dropDatabase()
 # time.sleep(1)
 testDB.setupDatabase("/lootius/database/lootiusTest.db")
-
